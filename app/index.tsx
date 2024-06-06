c2fdff6cbf617df1c7bd339900a32ccf53aa698c
Ariisss <arispantino@gmail.com>
2024-06-06 20:49:45 +0800

    feat: minor change

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet } from "react-native";
 const Main = () => {
     return (
         <View>
-            <Text>Hi, I'm a React Native app!</Text>
+            <Text></Text>
         </View>
     );
 };

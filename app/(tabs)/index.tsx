58f11328a1825154e7e5272a78ca9bb756a376df
Ariisss <arispantino@gmail.com>
2024-06-18 19:14:53 +0800

    feat: tested global styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,10 @@
 import { View,Text, SafeAreaView } from "react-native";
-import { colors } from "@/src/utils/GlobalStyles";
+import { colors, globalStyles } from "@/src/utils/GlobalStyles";
 
 
 export default function HomeScreen(){
     return(
-        <SafeAreaView style={{flex: 1, backgroundColor: colors.purple}}>
+        <SafeAreaView style={globalStyles.topCenterView}>
             <Text>hi</Text>
             <Text>Hello</Text>
         </SafeAreaView>
